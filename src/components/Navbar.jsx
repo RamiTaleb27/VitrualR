8d01a85eed2a802c72a4915833973b756d17eaf3
RamiTaleb27 <rami.m.taleb@gmail.com>
2024-08-28 11:54:31 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,10 +41,10 @@ const Navbar = () => {
                 })}
               </ul>
               <div className="flex space-x-6">
-                <a href="" className='py-2 px-3 border rounded-md'>
+                <a href="#" className='py-2 px-3 border rounded-md'>
                   Sign In
                 </a>
-                <a href="" className='py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800'>
+                <a href="#" className='py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800'>
                   Create an account
                 </a>
               </div>
